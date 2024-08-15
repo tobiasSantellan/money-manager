10a75774089d3e060e58f6b43e08c59d58429828
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-15 17:50:29 -0300

    create dashboard layout with side navigation

---
+++ app/(routes)/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import DashboardHeader from "../_components/DashboardHeader";
+import SideNav from "../_components/SideNav";
+
+function DashboardLayout({ children }) {
+  return (
+    <div>
+      <div className="fixed md:w-64 hidden md:block">
+        <SideNav />
+      </div>
+      <div className="md:ml-64">
+        <DashboardHeader />
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
