ff7c3d8d8f7f59d99688fa8f90859e87277f3eaa
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-17 20:05:49 -0300

    correct typing error in directory path

--- app/(routes)/_components/SideNav.jsx
+++ app/(routes)/_components/SideNav.jsx
@@ -18,7 +18,7 @@ function SideNav() {
       id: 2,
       name: "Budget",
       icon: <PiggyBank />,
-      path: "/dashboard/budget",
+      path: "/dashboard/budgets",
     },
     {
       id: 3,
