15f340b5bf4c97a9cb87d31289e84655b635adb5
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-17 20:10:39 -0300

    user experience enhancement - check initial budgets and redirect

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,7 +1,33 @@
+"use client";
+import { db } from "@/utils/dbConfig";
 import DashboardHeader from "../_components/DashboardHeader";
 import SideNav from "../_components/SideNav";
+import { Budgets } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { eq } from "drizzle-orm";
+import { useRouter } from "next/navigation";
 
 function DashboardLayout({ children }) {
+  const { user } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
+
+  const checkUserBudgets = async () => {
+    const result = await db
+      .select()
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+    console.log(result);
+    if (result?.length === 0) {
+      router.replace("/dashboard/budgets");
+    }
+  };
+
   return (
     <div>
       <div className="fixed md:w-64 hidden md:block">
