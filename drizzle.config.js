552f183add1d925ff486814625b7ac71c5b12fa2
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-17 20:15:20 -0300

    protect database connection string

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.jsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:7F5LzeySIlEY@ep-cold-glade-a5g0j7dg.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 };
