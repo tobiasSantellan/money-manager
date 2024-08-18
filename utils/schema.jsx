1fb8fe0b4207effed3b02b1b53e05c55585b69a6
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-18 17:51:15 -0300

    add Expensess table for tracking expenses

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,9 +1,25 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  numeric,
+  pgTable,
+  serial,
+  varchar,
+} from "drizzle-orm/pg-core";
 
+// Table to store budget information.
 export const Budgets = pgTable("budgets", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
   amount: varchar("amount").notNull(),
   icon: varchar("icon"),
   createdBy: varchar("createdBy").notNull(),
+});
+
+// This table stores information about the expenses associated with each budget.
+export const Expensess = pgTable("expensess", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: numeric("amount").notNull().default(0),
+  budgetId: integer("budgetId").references(() => Budgets.id),
+  createdAt: varchar("createdAt").notNull(),
 });
