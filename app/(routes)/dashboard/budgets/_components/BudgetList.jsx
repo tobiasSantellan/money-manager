74f175891c755e39f39c4833f50f2893cd6846c2
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-19 22:58:17 -0300

    Implement budget list with loading placeholder and refresh

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,12 +1,14 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CreateBudget from "./CreateBudget";
 import { db } from "@/utils/dbConfig";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { desc, eq, getTableColumns, sql } from "drizzle-orm";
 import { Budgets, Expensess } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
+import BudgetItem from "./BudgetItem";
 
 function BudgetList() {
+  const [budgetList, setBudgetList] = useState([]);
   const { user } = useUser();
   useEffect(() => {
     user && getBudgetList();
@@ -31,14 +33,23 @@ function BudgetList() {
       .from(Budgets)
       .leftJoin(Expensess, eq(Budgets.id, Expensess.budgetId))
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
-      .groupBy(Budgets.id);
-
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id));
+    setBudgetList(result);
     console.log(result);
   };
   return (
     <div className="mt-7">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <CreateBudget />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <CreateBudget refreshData={() => getBudgetList()} />
+        {budgetList.length > 0
+          ? budgetList.map((budget, index) => <BudgetItem budget={budget} />)
+          : [1, 2, 3, 4, 5].map((item, index) => (
+              <div
+                key={index}
+                className="w-full bg-slate-200 rounded-lf h-[150px] animate-pulse"
+              ></div>
+            ))}
       </div>
     </div>
   );
