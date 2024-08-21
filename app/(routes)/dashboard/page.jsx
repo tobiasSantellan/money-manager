d3912b83db66cefd071f3b506b96ea7f3b2decdd
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-20 21:38:20 -0300

    Add basic Dashboard component with user greeting

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,10 +1,15 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { useUser } from "@clerk/nextjs";
 
 function Dashboard() {
+  const { user } = useUser();
   return (
-    <div>
-      {/* <UserButton /> */}
-      Dashboard
+    <div className="p-8">
+      <h2 className="font-bold text-2xl">Hi, {user?.fullName} 👋</h2>
+      <p className="text-gray-500">
+        Here's the breakdown of your finances. Let's help you reach your
+        financial goals.
+      </p>
     </div>
   );
 }
