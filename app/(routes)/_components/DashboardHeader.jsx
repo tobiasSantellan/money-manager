d9cc328e3c805ae9371ddf5709f162cd07e8e9b9
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-21 18:55:03 -0300

    deleted search bar

--- app/(routes)/_components/DashboardHeader.jsx
+++ app/(routes)/_components/DashboardHeader.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function DashboardHeader() {
   return (
     <div className="p-5 shadow-sm border-b flex justify-between">
-      <div>Search Bar</div>
+      <div></div>
       <div>
         <UserButton />
       </div>
