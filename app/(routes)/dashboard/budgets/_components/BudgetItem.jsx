2673eb4cd1940fc27cde37a51c85d7fc7e36721e
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-20 21:35:46 -0300

    Add function to calculate budget progress percentage

--- app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 import React from "react";
 
 function BudgetItem({ budget }) {
+  const calculateProgressPerc = () => {
+    const perc = (budget?.totalSpend / budget?.amount) * 100;
+    return perc.toFixed(2);
+  };
   return (
     <Link
       href={"/dashboard/expenses/" + budget?.id}
-      className="p-5 border rounded-lg hover:shadow-md cursor-pointer"
+      className="p-5 border rounded-lg hover:shadow-md cursor-pointer h-[170px]"
     >
       <div className="flex gap-2 items-center justify-between">
         <div className="flex gap-2 items-center">
@@ -29,7 +33,12 @@ function BudgetItem({ budget }) {
           </h2>
         </div>
         <div className="w-full bg-slate-300 h-2 rounded-full">
-          <div className="w-[40%] bg-primary h-2 rounded-full"></div>
+          <div
+            className=" bg-primary h-2 rounded-full"
+            style={{
+              width: `${calculateProgressPerc()}%`,
+            }}
+          ></div>
         </div>
       </div>
     </Link>
