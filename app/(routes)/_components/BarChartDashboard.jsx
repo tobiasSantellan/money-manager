8b810582db57a39daf6ea6762a91026a2cbe38ea
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-21 18:52:35 -0300

    implement bar chart for budget activity

---
+++ app/(routes)/_components/BarChartDashboard.jsx
@@ -0,0 +1,34 @@
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+function BarChartDashboard({ budgetList }) {
+  return (
+    <div className="p-5 border rounded-lg">
+      <h2 className="font-bold text-lg">Activity</h2>
+      <ResponsiveContainer width={"80%"} height={300}>
+        <BarChart
+          data={budgetList}
+          margin={{
+            top: 7,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="totalSpend" stackId="a" fill="#4CAF50" />
+          <Bar dataKey="amount" stackId="a" fill="#94f798" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default BarChartDashboard;
