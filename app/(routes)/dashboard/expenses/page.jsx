73abeda982d449bea7f11c891d5acce051aeb6e7
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-21 18:59:56 -0300

    implement Expenses page with expense list
    - Fetches all expenses from the database using Drizzle ORM.
    - Orders expenses by descending ID (most recent first).
    - Renders the ExpenseListTable component to display the list of expenses.

--- app/(routes)/dashboard/expenses/page.jsx
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import ExpenseListTable from "./_components/ExpenseListTable";
+import { db } from "@/utils/dbConfig";
+import { Expensess } from "@/utils/schema";
+import { desc, eq } from "drizzle-orm";
 
 function Expenses() {
-  return <div>Expenses</div>;
+  useEffect(() => {
+    getExpensesList();
+  }, []);
+
+  const [expensesList, setExpensesList] = useState([]);
+  const getExpensesList = async () => {
+    const result = await db
+      .select()
+      .from(Expensess)
+      .orderBy(desc(Expensess.id));
+    setExpensesList(result);
+    console.log(result);
+  };
+  return (
+    <div className="mt-5 p-5">
+      <h2 className="font-bold text-2xl">My Expenses</h2>
+      <ExpenseListTable expensesList={expensesList} />
+    </div>
+  );
 }
 
 export default Expenses;
