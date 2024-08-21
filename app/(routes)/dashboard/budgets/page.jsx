c0738bb8d8f95bcb5c09408cb569ce80fb805ee0
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-20 22:00:39 -0300

    changed text color

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -3,7 +3,7 @@ import BudgetList from "./_components/BudgetList";
 function Budgets() {
   return (
     <div className="p-10">
-      <h2 className="font-bold text-3xl text-primary">My Budgets</h2>
+      <h2 className="font-bold text-3xl">My Budgets</h2>
       <BudgetList />
     </div>
   );
