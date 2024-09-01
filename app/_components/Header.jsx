51a6222eecdb93babb626e6c2d2fedb84d4eb55c
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-09-01 18:31:01 -0300

    "redirect to dashboard if user is authenticated"

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,9 +3,17 @@ import { Button } from "@/components/ui/button";
 import { UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function Header() {
   const { user, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) return router.push("/dashboard");
+  }, [user]);
+
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <Image src={"/logo.svg"} width={160} height={100} alt="logo" />
